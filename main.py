3634e558217b182ae9ef1910e6cf8f555960cb86
Satish Maharjan <satish.mhrjn10@gmail.com>
2025-04-11 13:23:52 +0545

    test

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+options = Options()
+options.add_argument("--headless")  # Run Chrome in headless mode
+options.add_argument("--disable-gpu")  # Optional: disable GPU
+options.add_argument("--window-size=1920,1080")  # Optional: set window size
+
 # Set up the Chrome driver (make sure the path is correct if chromedriver isn't in PATH)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 
 # Open Google
 driver.get("https://www.google.com")
